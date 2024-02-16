18932958c88e97ccdbd43921a1aba03dfe9b2d76
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-02-16 19:31:41 -0300

    Clear test route

--- src/http/routes/create-poll.ts
+++ src/http/routes/create-poll.ts
@@ -1,14 +1,8 @@
 import type { FastifyRequest, FastifyReply, FastifyInstance } from 'fastify';
 import { prisma } from '../../lib/prima';
 import { z } from 'zod';
-import { Server } from 'http';
 
 export async function createPoll(app: FastifyInstance) {
-  // @ts-ignore: Package version diffrence
-  app.get('/', async (request: FastifyRequest, response: FastifyReply) => {
-    return { greeting: 'Hello from Fastify and TypeScript!' };
-  });
-
   // @ts-ignore: Package version diffrence
   app.post('/polls/create', async (req: FastifyRequest, res: FastifyReply) => {
     const createPollBody = z.object({
